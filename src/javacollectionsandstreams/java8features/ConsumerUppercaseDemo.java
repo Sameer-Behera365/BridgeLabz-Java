7fad7c5960ff130a2e8ff744d13c4fcf71f29f28
Sameer-Behera365 <sonubehera2024@gmail.com>
2026-02-06 10:53:49 +0530

    feat: add java8featuress module

---
+++ src/javacollectionsandstreams/java8features/ConsumerUppercaseDemo.java
@@ -0,0 +1,11 @@
+package javacollectionsandstreams.java8features;
+
+import java.util.*;
+import java.util.function.*;
+
+public class ConsumerUppercaseDemo {
+    public static void main(String[] args) {
+        Consumer<String> c = s->System.out.println(s.toUpperCase());
+        List.of("a","b","c").forEach(c);
+    }
+}
